8c505b16a4c4426b99df8de58c2f13404aaa3786
Guilherme <guilhermejp4@gmail.com>
2020-08-10 15:30:51 +0200

    8 - Trying to fix Rating not showing on frontend. No Sucess... Moving to validation part for now

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -26,8 +26,8 @@ namespace MvcMovie.Models
                         Title = "When Harry Met Sally",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
-                        Price = 7.99M,
-                        Rating = "R"
+                        Rating = "R",
+                        Price = 7.99M
                     },
 
                     new Movie
@@ -35,8 +35,8 @@ namespace MvcMovie.Models
                         Title = "Ghostbusters ",
                         ReleaseDate = DateTime.Parse("1984-3-13"),
                         Genre = "Comedy",
-                        Price = 8.99M,
-                        Rating = "R"
+                        Rating = "R",
+                        Price = 8.99M
                     },
 
                     new Movie
@@ -44,8 +44,8 @@ namespace MvcMovie.Models
                         Title = "Ghostbusters 2",
                         ReleaseDate = DateTime.Parse("1986-2-23"),
                         Genre = "Comedy",
-                        Price = 9.99M,
-                        Rating = "R"
+                        Rating = "R",
+                        Price = 9.99M
                     },
 
                     new Movie
@@ -53,8 +53,8 @@ namespace MvcMovie.Models
                         Title = "Rio Bravo",
                         ReleaseDate = DateTime.Parse("1959-4-15"),
                         Genre = "Western",
-                        Price = 3.99M,
-                        Rating = "R"
+                        Rating = "R",
+                        Price = 3.99M
                     }
                 );
                 context.SaveChanges();
